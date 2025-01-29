24ce571492b45ff8e72df019af86a928f0397e3c
s0ur4bhkumar <soul65917@gmail.com>
2025-01-29 11:19:16 +0530

    modify html and script

--- script.js
+++ script.js
@@ -10,51 +10,39 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt('Enter your choice, "rock","papper" or "scissor": ');
-    return choice.toLowerCase();
+let selection = '';
+
+function getHumanChoice(button){
+
+    selection = button;
+    return selection;
+        
 }
 
 let humanScore = 0, computerScore = 0;
 function playRound(ComputerChoice,HumanChoice){
     ComputerChoice = getComputerChoice();
-    HumanChoice = getHumanChoice();
-    // console.log(ComputerChoice);
-    // console.log(humanScore);
+    HumanChoice = selection;
+    console.log(HumanChoice);
+
     if ((HumanChoice == 'rock' & ComputerChoice == 'scissor')||(HumanChoice == 'papper' & ComputerChoice == 'rock')||
     (HumanChoice == 'scissor') & ComputerChoice == 'papper'){
 
+        alert('you win');
         ++humanScore;
-        // return alert('you won!');
 
     }
     else if ((ComputerChoice == 'rock' & HumanChoice == 'scissor')||(ComputerChoice == 'papper' & HumanChoice == 'rock')||
     (ComputerChoice == 'scissor') & HumanChoice == 'papper'){
 
         ++computerScore;
-        // return alert('you lose!');
 
     }
 }
 
-function playGame(){
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+// let play_box = document.querySelector('.play-box');
 
-    if (humanScore > computerScore){
-        alert('you won!');
-    }
-    else if (humanScore == computerScore){
-        alert("It's a draw");
-    }
-    else{
-        alert("you lose!")
-    }
-}
 
-playGame();
+
 
 
