28755f080eff8a9fa261beee9e071f208cc8ddd4
s0ur4bhkumar <soul65917@gmail.com>
2026-01-14 14:01:56 +0530

    define variables to be used

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-//initial score
+// **********************************variables**********************************
 
+let activePlayer;
+let player1;
+let player2;
 let userScore = 0;
 let computerScore = 0;
+const startButton = document.querySelector(".start");
+const content = document.querySelector(".content");
+const gameButtons = document.createElement("div");
+gameButtons.classList.add("gameButtons");
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissor = document.createElement("button");
+const rockImage = document.createElement("img");
+const paperImage = document.createElement("img");
+const scissorImage = document.createElement("img");
+
+// *********************************fucntions***********************************
 
 //computer choice function
-//
+
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
   if (num === 1) {
@@ -22,7 +37,12 @@ function getUserChoice() {
   return prompt("Enter your choice from ['rock','paper','scissor']:");
 }
 
+//switch player function
+
+function switchPlayer() {}
+
 //function to play a single round
+
 function playRound(userChoice, computerChoice) {
   userChoice = getUserChoice();
   computerChoice = getComputerChoice();
@@ -31,35 +51,19 @@ function playRound(userChoice, computerChoice) {
     (userChoice === "paper" && computerChoice === "scissor") ||
     (userChoice === "scissor" && computerChoice === "rock")
   ) {
-    // console.log(`you lost!, ${computerChoice} beats ${userChoice}`);
-    ++computerScore;
-    return `computer score:${computerScore}, human score:${userScore}`;
+    return ++computerScore;
+    // return `computer score:${computerScore}, human score:${userScore}`;
   } else if (
     (computerChoice === "rock" && userChoice === "paper") ||
     (computerChoice === "paper" && userChoice === "scissor") ||
     (computerChoice === "scissor" && userChoice === "rock")
   ) {
-    // console.log(`you won!, ${userChoice} beats ${computerChoice}`);
-    ++userScore;
-    return `computer score:${computerScore}, human score:${userScore}`;
+    return ++userScore;
+    // return `computer score:${computerScore}, human score:${userScore}`;
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    console.log(
-      `round ${i} => computer score:${computerScore}, human score:${userScore}`,
-    );
-  }
-  if (userScore > computerScore) {
-    console.log(`computer score: ${computerScore}, your score:${userScore}`);
-    return "you win";
-  } else if (userScore < computerScore) {
-    console.log(`computer score: ${computerScore}, your score:${userScore}`);
-    return "you lose";
-  } else {
-    console.log(`computer score: ${computerScore}, your score:${userScore}`);
-    return "it's a draw";
-  }
-}
+// *****************************DOM manipulation*********************************
+startButton.addEventListener("click", (e) => {
+  content.removeChild(e.target);
+});
